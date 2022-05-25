c71fbf4e6dc38a013b5fceb0db508057e8e35a88
stevenakni <stevenaknidev@gmail.com>
2022-05-25 16:07:22 +0300

    chore(up):(up)

--- src/app-settings.js
+++ src/app-settings.js
@@ -1,7 +1,7 @@
 function getAppSettings() {
     return {
-        urlMainnet: "https://multisender.nearspace.info/",
-        urlTestnet: "https://testnet.multisender.nearspace.info/",
+        urlMainnet: "https://lnc-multisender-final.herokuapp.com/",
+        urlTestnet: "https://lnc-multisender-final.herokuapp.com/",
         appNme: "LNC Token Multisender",
         appDescription: "Multisender sends LNC tokens to hundreds of NEAR addresses out in 1 single transaction.",
         github: "https://github.com/YellingOilbird/"
